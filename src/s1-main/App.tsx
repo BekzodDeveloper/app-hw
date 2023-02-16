8d289c53a282b103728f8eb76bcb4aaa358ea139
Bekzod <bkzd18kholdarov@gmail.com>
2023-02-16 17:59:18 +0500

    Done HW#3

--- src/s1-main/App.tsx
+++ src/s1-main/App.tsx
@@ -3,14 +3,17 @@ import s from './App.module.css'
 // import HW1 from '../s2-homeworks/hw01/HW1'
 // import {Task1} from "../microtasks/task1/task1";
 import HW2 from '../s2-homeworks/hw02/HW2';
+import HW3 from "../s2-homeworks/hw03/HW3";
+import HW1 from "../s2-homeworks/hw01/HW1";
 
 function App() {
     return (
         <div className={s.App}>
             {/*<Task1/>*/}
             {/*раскомментировать по ходу выполнения*/}
+            <HW1/>
             <HW2/>
-            {/*<HW3/>*/}
+            <HW3/>
             {/*<HW4/>*/}
 
             {/*при выполнении дз 5 и более - закомментировать здесь дз 1-4, так как они есть внутри дз 5*/}
